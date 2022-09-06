a03d4adedabeb8d48dcfaca8f02ec1c556c126e4
NikhilV552 <nikhil20165520@gmail.com>
2022-09-06 18:35:12 +0530

    Routing added , guards and instrument basic service

--- small-change/src/app/login-form/login-form.component.spec.ts
+++ small-change/src/app/login-form/login-form.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { LoginFormComponent } from './login-form.component';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let routerStub;
 
   beforeEach(async () => {
+    routerStub=jasmine.createSpyObj('router', ['navigate']);
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
+      declarations: [ LoginFormComponent ],
+      providers:[{provide:Router, useValue: routerStub}]
     })
     .compileComponents();
   });
---
+++ small-change/src/app/services/instrument.service.spec.ts
@@ -0,0 +1,102 @@
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { InstrumentCategory } from '../models/instrument-category';
+import { InstrumentPrice } from '../models/instrument-price';
+
+import { InstrumentService } from './instrument.service';
+
+describe('InstrumentService', () => {
+  let service: InstrumentService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(InstrumentService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should retrive all the instrument categories',fakeAsync(()=>{
+    service.categories=[
+      {
+        categoryId:'3',
+        categoryName:'Cat -2'
+      }
+    ]
+    let returnedCategories:InstrumentCategory[]=[];
+    service.getAllCategories().subscribe(data=> returnedCategories=data);
+    tick()
+    expect(returnedCategories.length).toBe(1)
+  }))
+
+  it('should return instrument pricing based on the category sent to the service',fakeAsync(()=>{
+    service.categories=[
+      {
+        categoryId:'3',
+        categoryName:'Cat -2'
+      },
+      {
+        categoryId:'4',
+        categoryName:'Cat -4'
+      }
+    ]
+    service.instruments=[
+      {
+        instrumentId:'123-123-098',
+        description:'Apple Inc. dummy' ,
+        externalIdType:'CUSIP',
+        externalId:'037833101',
+        categoryId:'3',
+        minQuantity:3 ,
+        maxQuantity:180
+      },
+      {
+        instrumentId:'678-123-457',
+        description:'Alphabet dummy' ,
+        externalIdType:'CUSIP',
+        externalId:'02079K104',
+        categoryId:'4',
+        minQuantity:4 ,
+        maxQuantity:20
+      },
+      {
+        instrumentId:'768-345-558',
+        description:'Alaska Air Group' ,
+        externalIdType:'CUSIP',
+        externalId:'011659104',
+        categoryId:'4',
+        minQuantity:4 ,
+        maxQuantity:200
+      }]
+
+      service.instrumentPrices  = [
+        {
+          'instrumentId':'123-123-098',
+          askPrice:56,
+          bidPrice:56.56,
+          timestamp:new Date(Date.now()),
+          instrument:service.instruments[0],
+        },
+        {
+          'instrumentId':'678-123-457',
+          askPrice:48,
+          bidPrice:49.56,
+          timestamp:new Date(Date.now()),
+          instrument:service.instruments[1],
+        },
+        {
+          'instrumentId':'768-345-558',
+          askPrice:121,
+          bidPrice:124.56,
+          timestamp:new Date(Date.now()),
+          instrument:service.instruments[2],
+        }]
+
+      let returnedInstruments: InstrumentPrice[]=[]
+      service.getInstrumentsByCategory('3').subscribe(data=> returnedInstruments=data);
+      tick()
+      expect(returnedInstruments.length).toEqual(1)
+      expect(returnedInstruments[0].instrument.instrumentId).toBe('123-123-098')
+
+  }))
+});
